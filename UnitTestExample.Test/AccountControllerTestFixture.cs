2a1a1b59ee288189d7e70d7243f7a1abef4a8168
András Weisz <andras.weisz@stud.uni-corvinus.hu>
2022-11-25 11:02:09 +0100

    Jelszo validalas

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,68 @@ namespace UnitTestExample.Test
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+            TestCase("abcd1234", false),
+    TestCase("abcd", false),
+    TestCase("ABCD1234", false),
+    TestCase("abcdABCD1234", true)]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+        [
+    Test,
+    TestCase("irf@uni-corvinus", "Abcd1234"),
+    TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "abcd1234"),
+    TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+    TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+    TestCase("irf@uni-corvinus.hu", "Ab1234"),
+]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            // Assert
         }
     }
 }
